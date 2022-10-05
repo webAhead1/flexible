5745be16f79ee32462c7aee1318ee70811ce36c0
George Joubran <durd2001@gmail.com>
2022-10-05 20:41:10 +0300

    Fixed tailwind automatic compiling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="w-[250px] h-[250px] logo"
+      src="@/assets/logo.svg"
+    />
 
     <div class="wrapper">
       <nav>
