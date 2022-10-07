a97ff33bd8e26a97fff3ec44b44ad1ebeab3e9d1
Ramie Bashir <ramiebash@gmail.com>
2022-10-07 19:45:05 +0300

    styling done for titles and cards

--- src/views/QuestionnaireOrTemplates.vue
+++ src/views/QuestionnaireOrTemplates.vue
@@ -5,4 +5,17 @@
     <header>    
         <p>Choose what's right for you</p>
     </header>
+    <body>
+        <div class="questionnaire-or-templates-container">
+            <div class="build-website-option">
+                <div class="build-website-option-title">Build your own website</div>
+                <div class="build-website-card"></div>
+            </div>
+            <div class="or-container"><p>OR</p></div>
+            <div class="template-option">
+                <div class="template-option-title">Choose a template</div>
+                <div class="template-card"></div>
+            </div>
+        </div>
+    </body>
 </template>
