e9cf753311c2a23713103dde4175d0b028450547
Ramie Bashir <ramiebash@gmail.com>
2022-10-07 14:49:16 +0300

    created questionnaire-or-templates branch

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,6 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="w-[250px] h-[250px] logo"
-      src="@/assets/logo.svg"
-    />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Welcome Initial Project</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  
   <RouterView />
 </template>
