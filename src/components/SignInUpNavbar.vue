c09f6627027fe2d075b2417f542f37854b74f459
Asmaalafe55 <asmaalafe55@gmail.com>
2022-10-07 22:32:23 +0300

    adding logo with flexible

--- src/components/SignInUpNavbar.vue
+++ src/components/SignInUpNavbar.vue
@@ -4,7 +4,12 @@
     <div
       class="font-myFont font-normal bg-navyColor rounded-b-2xl h-106 not-italic align-middle text-3xl text-white text-center m-auto leading-10"
     >
-      Flexible
+      <router-link :to="{ path: '/', hash: '#home' }">
+        <div class="logoWithFlexible">
+          <img src="../assets/img/pic1.png" class="logo" />
+          <span>Flexible</span>
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
