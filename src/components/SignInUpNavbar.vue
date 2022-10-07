6520a22c11de7a0f5fec9d46d25d176c777fe9b8
Asmaalafe55 <asmaalafe55@gmail.com>
2022-10-07 22:38:53 +0300

    adding css to the logo

--- src/components/SignInUpNavbar.vue
+++ src/components/SignInUpNavbar.vue
@@ -1,15 +1,11 @@
 <script></script>
 <template>
-  <div class="top-0 rounded-b-2xl h-106">
-    <div
-      class="font-myFont font-normal bg-navyColor rounded-b-2xl h-106 not-italic align-middle text-3xl text-white text-center m-auto leading-10"
-    >
-      <router-link :to="{ path: '/', hash: '#home' }">
-        <div class="logoWithFlexible">
-          <img src="../assets/img/pic1.png" class="logo" />
-          <span>Flexible</span>
-        </div>
-      </router-link>
-    </div>
+  <div class="menu">
+    <router-link :to="{ path: '/', hash: '#home' }">
+      <div class="logoWithFlexible">
+        <img src="../assets/img/pic1.png" class="logo" />
+        <span>Flexible</span>
+      </div>
+    </router-link>
   </div>
 </template>
