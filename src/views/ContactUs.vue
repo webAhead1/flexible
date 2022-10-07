6a8ead5710cba996c986e962def4210c7866afc0
Ramie Bashir <ramiebash@gmail.com>
2022-10-07 22:51:29 +0300

    added background color gradient and built the structure of the contact card

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -10,7 +10,10 @@
             Name <br />
             <input v-model="text" class="name-input"/> <br />
             Email <br />
-            <input v-model="text" /> <br />
+            <input v-model="text" class="email-input" /> <br />
+            Tell us how we can help <br />
+            <input v-model="text" class="message-input" /> <br />
+            <button class="submit-button">Submit</button>
         </div>
     </body>
 </template>
