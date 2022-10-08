3fe7fb346d9bc2416f3db3a2091f40e49149acb1
Asmaalafe55 <asmaalafe55@gmail.com>
2022-10-08 21:25:04 +0300

    linking the sign in with the sign up, adding css to sign in\up

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -5,16 +5,12 @@
   <body>
     <div class="flex justify-center py-8 drop-shadow-xl">
       <div
-        class="bg-[#2B4F6087] w-[550px] h-[940px] rounded-[20px] drop-shadow-xl"
+        class="bg-[#2B4F6087] w-[600px] h-[690px] rounded-[20px] drop-shadow-xl"
       >
-        <div class="bg-[#2B4F6087] rounded-t-[20px] h-[70px]">
-          <div
-            class="flex justify-center text-white font-myFont pt-3 text-[34px]"
-          >
-            Sign In
-          </div>
+        <div class="signBg">
+          <div class="signText">Sign In</div>
           <div class="flex justify-center">
-            <div class="text-[30px] pt-6 font-myFont">
+            <div class="text-[30px] pt-16 font-myFont">
               <div class="txt">Email</div>
               <input
                 type="email"
@@ -25,14 +21,15 @@
               />
               <div class="txt">Password</div>
               <input type="password" id="password" name="password" required />
-              <div class="flex justify-center pt-6">
+              <div class="btnBg pt-14">
                 <div class="btn">
                   <button>Submit</button>
                 </div>
               </div>
-              <div class="pt-7 text-center text-[20px] text-white">
+              <div class="accountQuestion pt-8">
                 Don’t have an account yet? Click
-                <a class="text-[#013565F0]" href="#">here</a> to sign up
+                <RouterLink class="hereStyle" to="/sign-up">here</RouterLink>
+                to sign up
               </div>
             </div>
           </div>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -5,14 +5,10 @@
   <body>
     <div class="flex justify-center py-8 drop-shadow-xl">
       <div
-        class="bg-[#2B4F6087] w-[550px] h-[940px] rounded-[20px] drop-shadow-xl"
+        class="bg-[#2B4F6087] w-[600px] h-[940px] rounded-[20px] drop-shadow-xl"
       >
-        <div class="bg-[#2B4F6087] rounded-t-[20px] h-[70px]">
-          <div
-            class="flex justify-center text-white font-myFont pt-3 text-[34px]"
-          >
-            Sign Up
-          </div>
+        <div class="signBg">
+          <div class="signText">Sign Up</div>
           <div class="flex justify-center">
             <div class="text-[30px] pt-6 font-myFont">
               <div class="txt">First Name</div>
@@ -43,14 +39,15 @@
               <input type="password" id="password" name="password" required />
               <div class="txt">Confirm Password</div>
               <input type="password" id="cPassword" name="cPassword" required />
-              <div class="flex justify-center pt-6">
+              <div class="btnBg pt-2">
                 <div class="btn">
                   <button>Submit</button>
                 </div>
               </div>
-              <div class="pt-7 text-center text-[20px] text-white">
+              <div class="accountQuestion pt-4">
                 Already have an account? Click
-                <a class="text-[#013565F0]" href="#">here</a> to sign in
+                <RouterLink class="hereStyle" to="/sign-in">here</RouterLink>
+                to sign in
               </div>
             </div>
           </div>
