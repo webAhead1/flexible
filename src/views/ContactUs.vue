da0c0d0bad41106794ccc10c891579fb5243db93
Ramie Bashir <ramiebash@gmail.com>
2022-10-08 12:35:16 +0300

    completed the design for the contact us section

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -7,12 +7,12 @@
     </header>
     <body>
         <div class="contact-card">
-            Name <br />
+            <div class="contact-name-label">Name</div>
             <input v-model="text" class="name-input"/> <br />
-            Email <br />
+            <div class="contact-email-label">Email</div>
             <input v-model="text" class="email-input" /> <br />
-            Tell us how we can help <br />
-            <input v-model="text" class="message-input" /> <br />
+            <div class="contact-message-label">Tell us how we can help</div>
+            <textarea v-model="text" class="message-input" /> <br />
             <button class="submit-button">Submit</button>
         </div>
     </body>
