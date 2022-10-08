25f2578f1a7375ad843f4dfeea959a5024343cb9
Asmaalafe55 <asmaalafe55@gmail.com>
2022-10-08 18:09:14 +0300

    adding the sign in view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import SignUp from '../views/SignUp.vue';
+import SignIn from '../views/SignIn.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,14 +9,19 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
       path: '/sign-up',
       name: 'signup',
-      component: SignUp
+      component: SignUp,
     },
-  ]
+    {
+      path: '/sign-in',
+      name: 'signin',
+      component: SignIn,
+    },
+  ],
 });
 
 export default router;
---
+++ src/views/SignIn.vue
@@ -0,0 +1,51 @@
+<template>
+  <header>
+    <SignInUpNavbar />
+  </header>
+  <body>
+    <div class="flex justify-center py-8 drop-shadow-xl">
+      <div
+        class="bg-[#2B4F6087] w-[550px] h-[940px] rounded-[20px] drop-shadow-xl"
+      >
+        <div class="bg-[#2B4F6087] rounded-t-[20px] h-[70px]">
+          <div
+            class="flex justify-center text-white font-myFont pt-3 text-[34px]"
+          >
+            Sign In
+          </div>
+          <div class="flex justify-center">
+            <div class="text-[30px] pt-6 font-myFont">
+              <div class="txt">Email</div>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                placeholder="Email"
+              />
+              <div class="txt">Password</div>
+              <input type="password" id="password" name="password" required />
+              <div class="flex justify-center pt-6">
+                <div class="btn">
+                  <button>Submit</button>
+                </div>
+              </div>
+              <div class="pt-7 text-center text-[20px] text-white">
+                Don’t have an account yet? Click
+                <a class="text-[#013565F0]" href="#">here</a> to sign up
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</template>
+<script>
+import SignInUpNavbar from '../components/SignInUpNavbar.vue';
+export default {
+  components: {
+    SignInUpNavbar,
+  },
+};
+</script>
