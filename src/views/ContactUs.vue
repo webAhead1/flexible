016659d18f4c10d839eb9f5cd3a32ba492215429
Ramie Bashir <ramiebash@gmail.com>
2022-10-08 15:47:59 +0300

    added left and right padding to the contact card and enlarged the submit button slightly

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -8,11 +8,11 @@
     <body>
         <div class="contact-card">
             <div class="contact-name-label">Name</div>
-            <input v-model="text" class="name-input"/> <br />
+            <input class="name-input"/> <br />
             <div class="contact-email-label">Email</div>
-            <input v-model="text" class="email-input" /> <br />
+            <input class="email-input" /> <br />
             <div class="contact-message-label">Tell us how we can help</div>
-            <textarea v-model="text" class="message-input" /> <br />
+            <textarea class="message-input" /> <br />
             <button class="submit-button">Submit</button>
         </div>
     </body>
