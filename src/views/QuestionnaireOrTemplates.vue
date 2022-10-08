9661038ce949b988cf1271dd18387047f778756d
Ramie Bashir <ramiebash@gmail.com>
2022-10-08 19:02:23 +0300

    made the representative pictures smaller and added padding to the div cards

--- src/views/QuestionnaireOrTemplates.vue
+++ src/views/QuestionnaireOrTemplates.vue
@@ -9,12 +9,12 @@
         <div class="questionnaire-or-templates-container">
             <div class="build-website-option">
                 <div class="build-website-option-title">Build your own website</div>
-                <div class="build-website-card"></div>
+                <div class="build-website-card"><img class="questionnaire-img" src="../../public/icons/questionnaire.png"></div>
             </div>
             <div class="or-container"><p>OR</p></div>
             <div class="template-option">
                 <div class="template-option-title">Choose a template</div>
-                <div class="template-card"></div>
+                <div class="template-card"><img class="template-img" src="../../public/icons/template.png"></div>
             </div>
         </div>
     </body>
