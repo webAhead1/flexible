7042c744517cee4d843867a8f2d77801eae0b936
Ramie Bashir <ramiebash@gmail.com>
2022-10-10 17:04:37 +0300

    added a modal popup that thanks the user for their feedback and informs them that their request is under review

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,4 +1,44 @@
-<script>
+<script setup>
+    window.onload = function() {
+        const openModalButton = document.querySelector('[data-modal-target]')
+        const closeModalButtons = document.querySelectorAll('[data-close-button]')
+        const modal = document.querySelector('.modal')
+        const overlay = document.getElementById('overlay')
+        let nameInput = document.getElementById('name-input').value
+        let emailInput = document.getElementById('email-input').value
+        let messageInput = document.getElementById('message-input').value
+
+        console.log(nameInput)
+        console.log(emailInput)
+        console.log(messageInput)
+
+        openModalButton.addEventListener('click', openModal)
+
+        closeModalButtons.forEach(button => {
+            button.addEventListener('click',()=> {
+                const modal = button.closest('.modal') 
+                closeModal (modal)
+            })
+        })
+
+        function openModal() {
+            console.log(nameInput)
+            console.log(emailInput)
+            console.log(messageInput)
+            // if (nameInput != '' && emailInput != '' && messageInput != '') {
+                modal.classList.add('active')
+                overlay.classList.add('active')
+            // }
+            // else {
+            //     alert("You must fill out all the boxes")
+            // }
+        }
+
+        function closeModal() {
+            modal.classList.remove('active')
+            overlay.classList.remove('active')
+        }
+    }
 </script>
 
 <template>
@@ -8,12 +48,25 @@
     <body>
         <div class="contact-card">
             <div class="contact-name-label">Name</div>
-            <input class="name-input"/> <br />
+            <input id="name-input" class="name-input" /> <br />
             <div class="contact-email-label">Email</div>
-            <input class="email-input" /> <br />
+            <input id="email-input" class="email-input" /> <br />
             <div class="contact-message-label">Tell us how we can help</div>
-            <textarea class="message-input" /> <br />
-            <button class="submit-button">Submit</button>
+            <textarea id="message-input" class="message-input" /> <br />
+            <button data-modal-target="#modal" class="submit-button">Submit</button>
         </div>
+        <div class="modal" id="modal">
+            <div class="modal-header">
+                <div class="title">We appreciate your feedback</div>
+                <button data-close-button class="close-button">&times;</button>
+            </div>
+            <div class="modal-text">
+                We got your request and we'll get back to you as soon as possible.
+            </div>
+            <div data-close-button class="ok-button-container">
+                <button class="ok-button">Ok</button>
+            </div>
+        </div>
+        <div id="overlay"></div>
     </body>
 </template>
