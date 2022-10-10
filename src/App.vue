5102b83159520fef1338ce28149d1bd308505890
Basel <baselamin712@gmail.com>
2022-10-10 18:28:55 +0300

    crating new checkboxes component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import CheckBoxes from "./components/CheckBoxes.vue";
+</script>
+<script>
+export default {
+  components: CheckBoxes,
+};
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="w-[250px] h-[250px] logo"
-      src="@/assets/logo.svg"
-    />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Welcome Initial Project</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <CheckBoxes />
 </template>
