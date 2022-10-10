4e7e082c3a69d76eb9873a44ea59e75f2b6ac014
Rand <rand.mass9@gmail.com>
2022-10-10 18:27:18 +0300

    adding all work from samar rand and maysan

---
+++ src/components/StartPage.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { reactive } from "vue";
+
+const states = reactive({
+  data() {
+    return {
+      StartPageComponent: 1,
+    };
+  },
+});
+</script>
+
+<template>
+  <div class="flex rounded-[59px] h-[851px] w-[874px] bg-[#3E6273]">
+    <div
+      class="rounded-[10px] ml-[76px] mt-[42px] h-[766.46px] w-[721.38px] bg-[#C8DAE2]/[.67]"
+    >
+      <div
+        class="flex rounded-[37px] ml-[59px] mt-[87px] h-[121px] w-[604px] bg-[#849AA5]"
+      >
+        <h2
+          class="mt-[14px] text-center not-italic text-black font-poppins text-[33px] font-bold leading-[50px]"
+        >
+          You’re a few questions away from building the perfect website!
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Questionnaire.vue
+++ src/views/Questionnaire.vue
@@ -1,13 +1,58 @@
-<script>
-import StartPage from "../components/Template.vue";
-export default {
-  name: "Questionnaire",
-  components: { StartPage },
-};
+<script setup>
+import StartPage from "../components/StartPage.vue";
+import FirstQuestionPage from "../components/FirstQuestionPage.vue";
+import SecondQuestionPage from "../components/SecondQuestionPage.vue";
+import ThirdQuestionPage from "../components/ThirdQuestionPage.vue";
+import FourthQuestionPage from "../components/FourthQuestionPage.vue";
+import LastQuestionPage from "../components/LastQuestionPage.vue";
+import { reactive } from "vue";
+const states = reactive({
+  currentStep: 1,
+});
 </script>
-
 <template>
   <div>
-    <StartPage />
+    <StartPage v-if="states.currentStep == 1" />
+    <FirstQuestionPage v-else-if="states.currentStep == 2" />
+    <SecondQuestionPage v-else-if="states.currentStep == 3" />
+    <ThirdQuestionPage v-else-if="states.currentStep == 4" />
+    <FourthQuestionPage v-else-if="states.currentStep == 5" />
+    <LastQuestionPage v-else-if="states.currentStep == 6" />
+    <div class="flex justify-center">
+      <button
+        @click="states.currentStep--"
+        v-if="states.currentStep > 2"
+        class="shadow-[inset-0px_4px_4px_rgba(0, 0, 0, 0.25)] mt-[-171px] mr-[150px] rounded-[18px] w-[197px] h-[79px] bg-[#D11F1F] text-center not-italic text-black font-poppins text-[33px] font-normal leading-[50px]"
+      >
+        Back
+      </button>
+      <button
+        v-if="states.currentStep == 2"
+        class="shadow-[inset-0px_4px_4px_rgba(0, 0, 0, 0.25)] mt-[-171px] mr-[150px] rounded-[18px] w-[197px] h-[79px] bg-[#565454] text-center not-italic text-black font-poppins text-[33px] font-normal leading-[50px]"
+      >
+        Back
+      </button>
+      <button
+        @click="states.currentStep++"
+        v-if="states.currentStep == 1"
+        class="shadow-[inset-0px_4px_4px_rgba(0, 0, 0, 0.25)] mt-[-400px] rounded-[18px] w-[197px] h-[79px] bg-[#D11F1F] text-center not-italic text-black font-poppins text-[33px] font-normal leading-[50px]"
+      >
+        Start
+      </button>
+      <button
+        @click="states.currentStep++"
+        v-else-if="states.currentStep != 6"
+        class="shadow-[inset-0px_4px_4px_rgba(0, 0, 0, 0.25)] mt-[-171px] rounded-[18px] ml-[150px] w-[197px] h-[79px] bg-[#D11F1F] text-center not-italic text-black font-poppins text-[33px] font-normal leading-[50px]"
+      >
+        Next
+      </button>
+      <button
+        @click="alert('This is the last question')"
+        v-if="states.currentStep == 6"
+        class="shadow-[inset-0px_4px_4px_rgba(0, 0, 0, 0.25)] mt-[-171px] rounded-[18px] ml-[150px] w-[197px] h-[79px] bg-[#D11F1F] text-center not-italic text-black font-poppins text-[33px] font-normal leading-[50px]"
+      >
+        Submit
+      </button>
+    </div>
   </div>
 </template>
