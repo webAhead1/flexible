84b07f83ce4b6a56be1ae2b1e2524866346ed32f
Ramie Bashir <ramiebash@gmail.com>
2022-10-12 02:09:10 +0300

    background and div colors changed

--- src/views/QuestionnaireOrTemplates.vue
+++ src/views/QuestionnaireOrTemplates.vue
@@ -6,15 +6,16 @@
         <p>Choose what's right for you</p>
     </header>
     <body>
+        <div class="polygon"></div>
         <div class="questionnaire-or-templates-container">
             <div class="build-website-option">
                 <div class="build-website-option-title">Build your own website</div>
-                <div class="build-website-card"><img class="questionnaire-img" src="../../public/icons/questionnaire.png"></div>
+                <div class="build-website-card"></div>
             </div>
             <div class="or-container"><p>OR</p></div>
             <div class="template-option">
                 <div class="template-option-title">Choose a template</div>
-                <div class="template-card"><img class="template-img" src="../../public/icons/template.png"></div>
+                <div class="template-card"></div>
             </div>
         </div>
     </body>
