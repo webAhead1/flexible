ecae22a10e3313767180b2c64bcf6fc193f6d4d8
Basel <baselamin712@gmail.com>
2022-10-12 17:19:13 +0300

    check conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,10 @@
 module.exports = {
   content: ["./index.html", "./src/*/.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        poppins: ["Poppins"],
-      },
-      boxShadow: {
-        boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25)",
-      },
-    },
     colors: {
       backgroundColor: "#D7CEC7",
       text: "#989595",
+      white: "#FFFFFF",
       red: "#D11F1F",
       back: "#565454",
       black: "#000000",
