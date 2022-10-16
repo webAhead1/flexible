0e35eb2f54739d8429384fafead77ec5a0bff44e
Asmaalafe55 <asmaalafe55@gmail.com>
2022-10-16 19:49:43 +0300

    removing h1 element

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,13 @@
   <main>
     <Pricing id="pricing" />
     <QuestionnaireOrTemplates />
-    <h1 class="example" id="home">Welcome to the Home page</h1>
   </main>
 </template>
 
 <script>
 import Pricing from '../components/Pricing.vue';
 import Navbar from '../components/Navbar.vue';
-import QuestionnaireOrTemplates from '../views/QuestionnaireOrTemplates.vue';
+import QuestionnaireOrTemplates from '../components/QuestionnaireOrTemplates.vue';
 export default {
   components: {
     Pricing,
