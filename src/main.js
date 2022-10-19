47882e4219236907222f8b5415c72987f10b7ff0
Asmaalafe55 <asmaalafe55@gmail.com>
2022-10-20 01:17:25 +0300

    adding google oauth, buttons in sign in page

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./style.css";
-
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './style.css';
 const app = createApp(App);
+
+import gAuthPlugin from 'vue3-google-oauth2';
+let gauthClientId =
+  '905632566241-c0mi3crlia5q8h6tqe9uc2u647sjkpji.apps.googleusercontent.com';
+app.use(gAuthPlugin, {
+  clientId: gauthClientId,
+  scope: 'email',
+  prompt: 'consent',
+  fetch_basic_profile: false,
+});
 
 app.use(router);
 
-app.mount("#app");
+app.mount('#app');
--- src/views/SignInUp/SignIn.vue
+++ src/views/SignInUp/SignIn.vue
@@ -3,6 +3,30 @@
     <SignInUpNavbar />
   </header>
   <body>
+    <h1>Is Initialized: {{ Vue3GoogleOauth.isInit }}</h1>
+    <h1>Is Authorized: {{ Vue3GoogleOauth.isAuthorized }}</h1>
+    <h2 v-if="user">Logged in as:{{ user }}</h2>
+    <div class="btnBg pt-14">
+      <div class="btn">
+        <button
+          @click="handleSignIn"
+          :disabled="!Vue3GoogleOauth.isInit || Vue3GoogleOauth.isAuthorized"
+        >
+          Sign In
+        </button>
+      </div>
+    </div>
+    <div class="btnBg pt-14">
+      <div class="btn">
+        <button
+          @click="handleSignOut"
+          :disabled="!Vue3GoogleOauth.isAuthorized"
+        >
+          Log Out
+        </button>
+      </div>
+    </div>
+
     <div class="flex justify-center py-[153px]">
       <div class="signBg1 h-[690px]">
         <div class="signBg">
@@ -15,7 +39,7 @@
               <input type="password" id="password" name="password" required />
               <div class="btnBg pt-14">
                 <div class="btn">
-                  <button>Submit</button>
+                  <button>Sign In</button>
                 </div>
               </div>
               <div class="accountQuestion pt-8">
@@ -30,11 +54,49 @@
     </div>
   </body>
 </template>
+
 <script>
-import SignInUpNavbar from "@/components/SignInUpNavbar.vue";
+import { inject } from 'vue';
+import SignInUpNavbar from '@/components/SignInUpNavbar.vue';
 export default {
   components: {
     SignInUpNavbar,
+  },
+  setup() {
+    const Vue3GoogleOauth = inject('Vue3GoogleOauth');
+    return {
+      Vue3GoogleOauth,
+    };
+  },
+  data() {
+    return {
+      user: '',
+    };
+  },
+  methods: {
+    async handleSignIn() {
+      try {
+        const googleUser = await this.$gAuth.signIn();
+        console.log(this.$gAuth.signIn);
+        if (!googleUser) {
+          return null;
+        }
+        this.user = googleUser.getBasicProfile().getEmail();
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
+    },
+    async handleSignOut() {
+      try {
+        await this.$gAuth.signOut();
+        // console.log(this.$gAuth.signOut);
+        this.user = '';
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
+    },
   },
 };
 </script>
