d2c06a4fe96196ccee8d0870001e847ed88e99eb
Asmaalafe55 <asmaalafe55@gmail.com>
2022-10-20 23:19:50 +0300

    removing the old codremoving the old code

--- src/main.js
+++ src/main.js
@@ -2,18 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './style.css';
-import gAuthPlugin from 'vue3-google-oauth2';
 
 const app = createApp(App);
-
-let gauthClientId =
-  '905632566241-c0mi3crlia5q8h6tqe9uc2u647sjkpji.apps.googleusercontent.com';
-app.use(gAuthPlugin, {
-  clientId: gauthClientId,
-  scope: 'email',
-  prompt: 'consent',
-  fetch_basic_profile: false,
-});
 
 app.use(router);
 
--- src/views/SignInUp/SignIn.vue
+++ src/views/SignInUp/SignIn.vue
@@ -3,32 +3,6 @@
     <SignInUpNavbar />
   </header>
   <body>
-    <div>
-      <h1>IsInit: {{ Vue3GoogleOauth.isInit }}</h1>
-      <h1>IsAuthorized: {{ Vue3GoogleOauth.isAuthorized }}</h1>
-      <h2 v-if="user">signed user: {{ user }}</h2>
-      <div class="btnBg pt-14">
-        <div class="btn">
-          <button
-            @click="handleClickSignIn"
-            :disabled="!Vue3GoogleOauth.isInit || Vue3GoogleOauth.isAuthorized"
-          >
-            sign in
-          </button>
-        </div>
-      </div>
-      <div class="btnBg pt-14">
-        <div class="btn">
-          <button
-            @click="handleClickSignOut"
-            :disabled="!Vue3GoogleOauth.isAuthorized"
-          >
-            sign out
-          </button>
-        </div>
-      </div>
-    </div>
-
     <div class="flex justify-center py-[153px]">
       <div class="signBg1 h-[690px]">
         <div class="signBg">
@@ -59,7 +33,6 @@
 
 <script>
 import SignInUpNavbar from '@/components/SignInUpNavbar.vue';
-import { inject } from 'vue';
 
 export default {
   components: {
@@ -68,46 +41,6 @@ export default {
   data() {
     return {
       user: '',
-    };
-  },
-  methods: {
-    async handleClickSignIn() {
-      try {
-        const googleUser = await this.$gAuth.signIn();
-        if (!googleUser) {
-          return null;
-        }
-        console.log('googleUser', googleUser);
-        this.user = googleUser.getBasicProfile().getEmail();
-        console.log('getId', this.user);
-        console.log('getBasicProfile', googleUser.getBasicProfile());
-        console.log('getAuthResponse', googleUser.getAuthResponse());
-        console.log(
-          'getAuthResponse',
-          this.$gAuth.instance.currentUser.get().getAuthResponse()
-        );
-      } catch (error) {
-        //on fail do something
-        console.error(error);
-        return null;
-      }
-    },
-    async handleClickSignOut() {
-      try {
-        await this.$gAuth.signOut();
-        console.log('isAuthorized', this.Vue3GoogleOauth.isAuthorized);
-        this.user = '';
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  },
-  setup() {
-    const Vue3GoogleOauth = inject('Vue3GoogleOauth');
-    const handleClickLogin = () => {};
-    return {
-      Vue3GoogleOauth,
-      handleClickLogin,
     };
   },
 };
