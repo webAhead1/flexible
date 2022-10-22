13db1d4722452219c2e9411632fd6af20050fdba
Rand <rand.mass9@gmail.com>
2022-10-22 19:56:23 +0300

    start profile page

---
+++ src/views/Profile/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ user }}</h1>
+  <button @click="handleSignOut(e)">Sign Out</button>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      user: localStorage.getItem("user"),
+    };
+  },
+  methods: {
+    //     getUser(){
+    // user = localStorage.getItem("user");
+    //     }
+    handleSignOut(event) {
+      localStorage.removeItem("user");
+    },
+  },
+};
+</script>
