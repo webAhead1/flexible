683c0a9b7f53600fe75ff154a53302d19139a61a
Rand <rand.mass9@gmail.com>
2022-10-25 23:48:29 +0300

    added the subscribtions component to the profile

---
+++ src/components/ProfileComponent/Subscriptions.vue
@@ -0,0 +1,27 @@
+<script>
+import PricingCard from "@/components/Pricing/PricingCard.vue";
+export default {
+  components: {
+    PricingCard,
+  },
+};
+</script>
+<template>
+  <div class="flex justify-center space-x-11">
+    <div class="mt-14">
+      <PricingCard
+        :name="'Basic'"
+        :shekels="'shekels'"
+        :price="57"
+        :monthly="'monthly'"
+        :sentence="'For personal use'"
+        :offers="[
+          'Custom domain',
+          'Guest editors - 3',
+          'Storage space - 2 GB',
+          'Customer care - 24/7',
+        ]"
+      />
+    </div>
+  </div>
+</template>
