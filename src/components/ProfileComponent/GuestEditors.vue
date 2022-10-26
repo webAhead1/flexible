bdb98d014a0c234a293dd6e8169db4c3e3aa4523
Rand <rand.mass9@gmail.com>
2022-10-26 11:48:54 +0300

    finished the subcription component in the profile

--- src/components/ProfileComponent/GuestEditors.vue
+++ src/components/ProfileComponent/GuestEditors.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="settings-content-box">
-    <div class="setting-header">Editors</div>
-    <div class="setting-website">1. johndoe01@gmail.com</div>
-    <div class="setting-website">2. janedoe01@gmail.com</div>
+  <div class="relative">
+    <div class="settings-content-box">
+      <div class="setting-header">Editors</div>
+      <div class="setting-website">1. johndoe01@gmail.com</div>
+      <div class="setting-website">2. janedoe01@gmail.com</div>
+    </div>
   </div>
 </template>
