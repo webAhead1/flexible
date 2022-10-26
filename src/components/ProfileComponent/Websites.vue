f2f0ad27a54ac36dfc3a0be32bc64e593004de9f
Rand <rand.mass9@gmail.com>
2022-10-26 12:58:01 +0300

    finished the payment component in the profile page

--- src/components/ProfileComponent/Websites.vue
+++ src/components/ProfileComponent/Websites.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="relative">
-    <div class="settings-content-box">
-      <div class="setting-header">Websites</div>
-      <div class="setting-website">ebook.flexible.com</div>
-      <div class="setting-website">candles.flexible.com</div>
-      <div class="setting-website">websites.flexible.com</div>
-    </div>
+  <div class="settings-content-box">
+    <div class="setting-header">Websites</div>
+    <div class="setting-website">ebook.flexible.com</div>
+    <div class="setting-website">candles.flexible.com</div>
+    <div class="setting-website">websites.flexible.com</div>
   </div>
 </template>
---
+++ src/components/ProfileComponent/YourPayments.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="settings-content-box">
+    <div class="setting-header">Payments</div>
+    <div class="flex justify-center">
+      <table class="payment-table">
+        <thead>
+          <tr>
+            <th class="table-content">Date</th>
+            <th class="table-content">Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="table-content">24/07/22</td>
+            <td class="table-content">58</td>
+          </tr>
+          <tr>
+            <td class="table-content">24/08/22</td>
+            <td class="table-content">58</td>
+          </tr>
+          <tr>
+            <td class="table-content">24/09/22</td>
+            <td class="table-content">58</td>
+          </tr>
+          <tr>
+            <td class="table-content">24/10/22</td>
+            <td class="table-content">58</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
